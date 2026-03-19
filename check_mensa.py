538a320e9bcca5d6d5e3d7dfdd13c8e5ff503346
inoue0475 <inoue88@live.jp>
2026-03-19 19:26:35 +0900

    Update check_mensa.py

--- check_mensa.py
+++ check_mensa.py
@@ -1,11 +1,13 @@
 import requests
 
+print("START")
+
 WEBHOOK = "https://discord.com/api/webhooks/1484121065134358579/2Xk0eI4G7QS-Zg_5b3xEkf_ergLjx8ESS3l29Mz-lihql9_Sz0RR06vLKw_dQ-xtj0Zc"
 
 response = requests.post(
     WEBHOOK,
-    json={"content": "🔥 テスト通知（最終確認）"}
+    json={"content": "🔥 テスト通知（ログ確認）"}
 )
 
-print(response.status_code)
-print(response.text)
+print("STATUS:", response.status_code)
+print("END")

a81a7273b57f6d15c1dd89d0b623e2694eba8fc1
inoue0475 <inoue88@live.jp>
2026-03-19 19:14:40 +0900

    Update check_mensa.py

--- check_mensa.py
+++ check_mensa.py
@@ -1,6 +1,6 @@
 import requests
 
-WEBHOOK = "ここにDiscordのWebhook URLをそのまま貼る"
+WEBHOOK = "https://discord.com/api/webhooks/1484121065134358579/2Xk0eI4G7QS-Zg_5b3xEkf_ergLjx8ESS3l29Mz-lihql9_Sz0RR06vLKw_dQ-xtj0Zc"
 
 requests.post(
     WEBHOOK,

f510d5e11bb6fe83a714fbbc4cd38edd83a00e87
inoue0475 <inoue88@live.jp>
2026-03-19 19:30:27 +0900

    Update check_mensa.py

--- check_mensa.py
+++ check_mensa.py
@@ -1,13 +1,26 @@
 import requests
 
-print("START")
-
+URL = "https://mensa.jp/exam/"
 WEBHOOK = "https://discord.com/api/webhooks/1484121065134358579/2Xk0eI4G7QS-Zg_5b3xEkf_ergLjx8ESS3l29Mz-lihql9_Sz0RR06vLKw_dQ-xtj0Zc"
 
-response = requests.post(
-    WEBHOOK,
-    json={"content": "🔥 テスト通知（ログ確認）"}
-)
+OPEN_IMAGE = "entry_over.jpg"
+FULL_IMAGE = "entry_quota.jpg"
 
-print("STATUS:", response.status_code)
-print("END")
+def check():
+    res = requests.get(URL, timeout=10)
+    html = res.text
+
+    if OPEN_IMAGE in html:
+        return "OPEN"
+    if FULL_IMAGE in html:
+        return "FULL"
+    return "UNKNOWN"
+
+def notify(msg):
+    requests.post(WEBHOOK, json={"content": msg})
+
+if __name__ == "__main__":
+    status = check()
+
+    if status == "OPEN":
+        notify("🔥 MENSA募集開始！ https://mensa.jp/exam/")

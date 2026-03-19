cd0f84ce4d1c66d9922a71b2cf5205677182c82b
inoue0475 <inoue88@live.jp>
2026-03-19 19:21:47 +0900

    Update check_mensa.py

--- check_mensa.py
+++ check_mensa.py
@@ -1,13 +1,11 @@
 import requests
 
-# ↓ここにDiscordのWebhook URLをそのまま貼る
 WEBHOOK = "https://discord.com/api/webhooks/1484121065134358579/2Xk0eI4G7QS-Zg_5b3xEkf_ergLjx8ESS3l29Mz-lihql9_Sz0RR06vLKw_dQ-xtj0Zc"
 
-def notify():
-    requests.post(
-        WEBHOOK,
-        json={"content": "🔥 テスト通知：GitHubから送信成功"}
-    )
+response = requests.post(
+    WEBHOOK,
+    json={"content": "🔥 テスト通知（最終確認）"}
+)
 
-if __name__ == "__main__":
-    notify()
+print(response.status_code)
+print(response.text)

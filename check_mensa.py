03b9e3d824f9a5b0f7714a855ffdd00265473ca8
inoue0475 <inoue88@live.jp>
2026-03-19 19:17:44 +0900

    Update check_mensa.py

--- check_mensa.py
+++ check_mensa.py
@@ -1,1 +1,8 @@
+import requests
+
 WEBHOOK = "https://discord.com/api/webhooks/1484121065134358579/2Xk0eI4G7QS-Zg_5b3xEkf_ergLjx8ESS3l29Mz-lihql9_Sz0RR06vLKw_dQ-xtj0Zc"
+
+requests.post(
+    WEBHOOK,
+    json={"content": "テスト通知"}
+)
